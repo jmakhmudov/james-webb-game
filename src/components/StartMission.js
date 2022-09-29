7d08a6a2c1b395cdc8c217c3020e70dcb2a79cd0
Jaloliddin Makhmudov <makhmudov@tuta.io>
2022-09-29 15:46:47 +0500

    start bckg

--- src/components/StartMission.js
+++ src/components/StartMission.js
@@ -5,7 +5,11 @@ import { TypeAnimation } from 'react-type-animation';
 export default function StartMission() {
 
     return (
-        <div className="start-mission">
+        <div className="start-mission" style={{
+            background: `url('${process.env.PUBLIC_URL}/images/bc23.png')`,
+            backgroundPosition: "bottom center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"}}>
             <p className="start-btn">START MISSION</p>
             <p className="onegate">powered by <span>ONEGATE</span></p>
         </div>
