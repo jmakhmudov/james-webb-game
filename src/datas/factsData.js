2f62bf52dd46d8fab6e5fe30f944e8cce36e9da0
Jaloliddin Makhmudov <makhmudov@tuta.io>
2022-09-30 10:53:49 +0500

    factsData creating

---
+++ src/datas/factsData.js
@@ -0,0 +1,49 @@
+export default facts = [
+    {
+        id: 1,
+        question: "DLJFDFDFJDKF?",
+        imgName: "NAME.png",
+        answers: [
+            {
+                description: "asdf",
+                correct: true
+            },
+            {
+                description: "asdf",
+                correct: false
+            },
+            {
+                description: "asdf",
+                correct: false
+            },
+            {
+                description: "asdf",
+                correct: false
+            },
+        ]
+    },
+
+    {
+        id: 2,
+        question: "DLJFDFDFJDKF?",
+        imgName: "NAME.png",
+        answers: [
+            {
+                description: "asdf",
+                correct: true
+            },
+            {
+                description: "asdf",
+                correct: false
+            },
+            {
+                description: "asdf",
+                correct: false
+            },
+            {
+                description: "asdf",
+                correct: false
+            },
+        ]
+    },
+]
